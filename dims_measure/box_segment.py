55cc08ad798825d5fe7011da9979e53123ee4c9b
Mike Chen <mikecpp.chen@gmail.com>
2024-02-29 16:14:52 +0800

    add text 3d

--- dims_measure/box_segment.py
+++ dims_measure/box_segment.py
@@ -1,6 +1,7 @@
 import open3d as o3d
 import numpy as np
 import open3d.visualization as visual
+from text_3d import text_3d
 
 VOXEL_SIZE =  0.001
 
@@ -18,14 +19,14 @@ points = points[points[:, 0] >= -0.30]
 pcd.points = o3d.utility.Vector3dVector(points)
 # o3d.visualization.draw_geometries([pcd]) 
 
-### Get table plane 
+### Get table top plane 
 plane_model, inliers = pcd.segment_plane(distance_threshold=0.01, ransac_n=3, num_iterations=10000)
 [a, b, c, d1] = plane_model
 table_pcd = pcd.select_by_index(inliers)
 table_pcd.paint_uniform_color([1.0, 0, 0]) 
 other_pcd = pcd.select_by_index(inliers, invert=True)
 
-### Get box plane 
+### Get box top plane 
 plane_model, inliers = other_pcd.segment_plane(distance_threshold=0.015, ransac_n=3, num_iterations=10000)
 [a, b, c, d2] = plane_model
 
@@ -35,7 +36,7 @@ box_pcd = other_pcd.select_by_index(inliers)
 
 ### Pick up any box point plus depth 
 points = np.asarray(box_pcd.points) 
-points[-100, 2] -= depth
+points[-100, 2] -= (depth - .005)
 box_pcd.points = o3d.utility.Vector3dVector(points)
 box_pcd.paint_uniform_color([0, 0, 1.0]) 
 
@@ -43,6 +44,10 @@ box_pcd.paint_uniform_color([0, 0, 1.0])
 bbox = box_pcd.get_axis_aligned_bounding_box() 
 bbox.color = (0, 1, 0) 
 bound = bbox.max_bound - bbox.min_bound
-print(f"Width: {bound[0]*1000:.0f}mm, Length: {bound[1]*1000:.0f}mm, Height: {bound[2]*1000:.0f}mm") 
 
-o3d.visualization.draw_geometries([pcd, bbox]) 
+msg = f"W: {bound[0]*1000:.0f}mm, L: {bound[1]*1000:.0f}mm, H: {bound[2]*1000:.0f}mm"
+print(msg)
+
+pcd_text = text_3d(msg, pos=bbox.get_box_points()[6], font_size=15)
+
+o3d.visualization.draw_geometries([pcd, bbox, pcd_text]) 
